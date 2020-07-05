d69ecd7a5912f3e6a8f11eadc768c5c7ee7f9e23
Ali Alamsyah Harahap <61445906+alialamsyah@users.noreply.github.com>
2020-07-05 20:58:03 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
-<?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
 |
 */
+
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,4 +13,20 @@ Route::get('/items', 'ItemController@index'); // menampilkan semua
 Route::get('/items/{id}', 'ItemController@show'); // menampilkan detail item dengan id 
 Route::get('/items/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
 Route::put('/items/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
-Route::delete('/items/{id}', 'ItemController@destroy'); // menghapus data dengan id
+Route::delete('/items/{id}', 'ItemController@destroy'); // menghapus data dengan id 
+
+
+Route::get('/artikel', 'ArtikelController@index');
+Route::get('/artikel/create', 'ArtikelController@create');
+Route::post('/artikel', 'ArtikelController@store');
+Route::get('/artikel/{id}', 'ArtikelController@detil');
+Route::get('/artikel/{id}/edit', 'ArtikelController@edit');
+Route::put('/artikel/{id}', 'ArtikelController@storeEdit');
+Route::delete('/artikel/{id}', 'ArtikelController@hapus'); 
+// Route::get('/items/create', 'ItemController@create'); // menampilkan halaman form
+// Route::post('/items', 'ItemController@store'); // menyimpan data
+// Route::get('/items', 'ItemController@index'); // menampilkan semua
+// Route::get('/items/{id}', 'ItemController@show'); // menampilkan detail item dengan id 
+// Route::get('/items/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
+// Route::put('/items/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
+// Route::delete('/items/{id}', 'ItemController@destroy'); // menghapus data dengan id 
